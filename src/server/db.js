47e857f7f6f03dca3ade71daeb7b005328a65014
Josh Bavari <jbavari@gmail.com>
2015-11-23 13:44:14 -0700

    Adding new database scripts. Insert script updated. Changed DDL to use position. Updated controllers for node, included pg package.

---
+++ src/server/db.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://JoshBavari@localhost:5432/stem_events';
+// postgres://username:password@localhost/database
+var client = new pg.Client(connectionString);
+client.connect();
+// var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+// query.on('end', function() { client.end(); });
+
+console.log('Database connection established')
+module.exports = client;
