39fc37779fb1d10cd5c4fa65b92814e49eab9fab
JENNIFER WADELLA <jennifer.wadella@gmail.com>
2015-11-30 13:25:33 -0600

    changing routes

--- src/client/app.config.js
+++ src/client/app.config.js
@@ -1,20 +1,19 @@
 'use strict'
 
 export default function appConfig($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
 
-	$locationProvider.html5Mode(true);
-
-	$stateProvider
-		.state('app', {
-			url: '/',
-			abstract: true,
-			template: require('./app/home/views/home.html')
-		})
-		.state('app.calendar', {
-			url: 'calendar',
-			controller: 'CalendarCtrl as cv',
-			template: require('./app/home/views/calendar.html')
-		});
+   $stateProvider
+      .state('app', {
+         url: '',
+         abstract: true,
+         template: require('./app/home/views/home.html')
+      })
+      .state('app.calendar', {
+         url: 'calendar',
+         controller: 'CalendarCtrl as cv',
+         template: require('./app/home/views/calendar.html')
+      });
 }
 
 appConfig.$inject = ['$stateProvider', '$locationProvider']
